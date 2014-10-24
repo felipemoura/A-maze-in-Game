14b7544e3ee5ad9bbc19f3b54e20782c07520339
Stanou01260 <stanislas.heili@gmail.com>
2014-10-23 23:59:21 -0500

    Maze generation
    Working (exept display)

---
+++ Maze/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    int maze[30][30];
+    int x[20][20];
+    int y[20][20];
+    int i,j;
+    int length;
+    int height;
+    int totalCells;
+
+
+
+private:
+    Ui::MainWindow *ui;
+    void MainWindow::workOnCell(int,int);
+};
+
+#endif // MAINWINDOW_H
